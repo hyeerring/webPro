a60e09dcec3edebfa6c40a33a2be100c24c28471
hyeerring <hyeeeerinnnn@gmail.com>
2023-02-07 16:52:57 +0900

    javaScript_ch01

---
+++ 04_JavaScript/ch01/03.js
@@ -0,0 +1,7 @@
+let sum = 0;
+    for(var i=1; i<=10; i++){
+        sum += i;
+        console.log('i= ' + i + '일 때까지 누적된 합은 ' + sum);
+    }
+    console.log('for문 끝')
+    console.log(i + "까지 누적된 최종 합은 " + sum);
